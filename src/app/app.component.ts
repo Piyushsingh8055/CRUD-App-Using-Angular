4bba58bfb1e98400b36b68b94b8afcbd313d7da0
Piyushsingh8055 <bosspiyush98@gmail.com>
2023-07-13 18:37:32 +0530

    c

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { EmpAddEditComponent } from './emp-add-edit/emp-add-edit.component';
+import { EmployeeService } from './services/employee.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'CRUD-App';
+
+  constructor(private _dialog: MatDialog, 
+              private _empService: EmployeeService){}
+              ngOnInit(): void {
+                  this.getEployeeList();
+              }
+
+  openAddEditEmpForm(){
+    this._dialog.open(EmpAddEditComponent);
+  }
+  getEployeeList(){
+    this._empService.getEployeeList().subscribe({
+      next:(res)=>{
+        console.log(res);
+    },
+    error:console.log,
+    });
+  }
 }
